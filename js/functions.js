4b8e36b5b5198bab5651d757dfea9b16e5b37e46
Виктория Клименко <climenkov6@gmail.com>
2025-09-17 16:40:28 +0500

    2.20. Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -8,13 +8,10 @@ const isPalindrome = function (string) {
   return cleanString === reversedString;
 };
 
-console.log('Проверка длины строки:');
-console.log(checkStringLength('проверяемая строка', 20));
-console.log(checkStringLength('проверяемая строка', 18));
-console.log(checkStringLength('проверяемая строка', 10));
+checkStringLength('проверяемая строка', 20);
+checkStringLength('проверяемая строка', 18);
+checkStringLength('проверяемая строка', 10);
 
-console.log('\nПроверка палиндромов:');
-console.log(isPalindrome('топот'));
-console.log(isPalindrome('ДовОд'));
-console.log(isPalindrome('Кекс'));
-console.log(isPalindrome('Лёша на полке клопа нашёл '));
+isPalindrome('топот');
+isPalindrome('ДовОд');
+isPalindrome('Кекс');
